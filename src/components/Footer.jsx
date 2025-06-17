da4bd791e0964b38dcf57cad55dd17720988a96a
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-06-17 14:59:25 -0300

    feat: create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="mt-6 p-4 bg-gray-900">
+            <div className="mx-auto max-w-screen-xl">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Suporte e Atendimento</h2>
+                            <ul className="text-gray-600 dark:text-gray-400">
+                                <li className="mb-4 flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 text-gray-200">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                                    </svg>
+                                    <a href="#" className="pl-2 hover:underline">+55 31 12345-6789</a>
+                                </li>
+                                <li className='mb-4 flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 text-gray-200">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                    </svg>
+                                    <a href="#" className="pl-2 hover:underline">contato.jagrifes@gmail.com</a>
+                                </li>
+                                <li className='mb-4 flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 text-gray-200">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
+                                    <a href="#" className="pl-2 hover:underline">Belo Horizonte, Brasil</a>
+                                </li>
+                                <li className='inline-flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 text-gray-200">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                    </svg>
+                                    <a href="#" className="pl-2 hover:underline">Segunda-Sábado: 9:00 - 18:00</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-2">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Sobre Nós</h2>
+                            <ul className="text-gray-600 dark:text-gray-400">
+                                <li className="mb-4">
+                                    <a href="#" className="hover:underline hover:text-gray-200">Minha Conta</a>
+                                </li>
+                                <li className="mb-4">
+                                    <a href="#" className="hover:underline hover:text-gray-200">Fale Conosco</a>
+                                </li>
+                                <li className="mb-4">
+                                    <a href="#" className="hover:underline hover:text-gray-200">Lista de Desejos</a>
+                                </li>
+                                <li className="mb-4">
+                                    <a href="#" className="hover:underline hover:text-gray-200">Meu Carrinho</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:underline hover:text-gray-200">Termo e Condições</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <div className='text-center my-3'>
+                    <span className="text-sm text-gray-500">© 2026 <a href="#" className="hover:underline">JA GRIFES™</a>. Todos Direitos Reservado.
+                    </span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import Navbar from './components/Navbar.jsx'
 import TopAnuncio from './components/TopAnuncio.jsx'
 import Lançamentos from './components/Lançamentos.jsx'
 import TimesBrasileiros from './components/TimesBrasileiros.jsx'
 import TimesInternacionais from './components/TimesInternacionais.jsx'
 import InfoLoja from './components/InfoLoja.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,6 +19,7 @@ createRoot(document.getElementById('root')).render(
     <TimesBrasileiros />
     <TimesInternacionais />
     <InfoLoja />
+    <Footer />
     
   </StrictMode>,
 )
