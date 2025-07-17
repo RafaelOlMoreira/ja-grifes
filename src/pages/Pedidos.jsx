d8c9f270ccd3465997eee0d4e0b912301fab278a
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-07-17 17:50:35 -0300

    feat: add animations to Order Cards

--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -85,7 +85,7 @@ function Pedidos() {
 
                 <div className='flex p-4 mb-4 justify-evenly bg-white shadow-lg rounded-b-xl rounded-t-0 overflow-hidden border border-gray-200'>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
@@ -93,7 +93,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                             </svg>
@@ -101,7 +101,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                             </svg>
@@ -183,7 +183,7 @@ function Pedidos() {
 
                 <div className='flex p-4 mb-4 justify-evenly bg-white shadow-lg rounded-b-xl rounded-t-0 overflow-hidden border border-gray-200'>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
@@ -191,7 +191,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                             </svg>
@@ -199,7 +199,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                             </svg>
@@ -281,7 +281,7 @@ function Pedidos() {
 
                 <div className='flex p-4 mb-4 justify-evenly bg-white shadow-lg rounded-b-xl rounded-t-0 overflow-hidden border border-gray-200'>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
@@ -289,7 +289,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                             </svg>
@@ -297,7 +297,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                             </svg>
@@ -379,7 +379,7 @@ function Pedidos() {
 
                 <div className='flex p-4 mb-4 justify-evenly bg-white shadow-lg rounded-b-xl rounded-t-0 overflow-hidden border border-gray-200'>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
@@ -387,7 +387,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                             </svg>
@@ -395,7 +395,7 @@ function Pedidos() {
                         </button>
                     </div>
                     <div>
-                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 hover:cursor-pointer'>
+                        <button type="button" className='flex p-3 px-16 items-center gap-2 bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200 hover:bg-gray-200 transition-all duration-300 hover:scale-105 hover:cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                             </svg>
